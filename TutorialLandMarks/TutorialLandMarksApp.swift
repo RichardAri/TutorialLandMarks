24ddd33efe04fcf6dd7e3026a54cd00b58f27cd6
RichardAri <richardaripari@gmail.com>
2024-03-20 19:30:32 -0500

    Initial Commit

---
+++ TutorialLandMarks/TutorialLandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialLandMarksApp.swift
+//  TutorialLandMarks
+//
+//  Created by Richard on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TutorialLandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
